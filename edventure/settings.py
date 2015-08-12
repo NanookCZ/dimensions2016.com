1fe1032d1febf13fed16c92c8da58d0f065ed0f0
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-12 17:01:59 +0200

    requirements

--- edventure/settings.py
+++ edventure/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_gravatar',
+    'disqus',
+    'endless_pagination',
+    'easy_maps',
+    'bootstrap3_datetime',
 )
 
 MIDDLEWARE_CLASSES = (

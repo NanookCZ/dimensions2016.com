224a8f85de0d5629b2768ddc0adfa24d1a791ea8
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-12 16:58:34 +0200

    database

--- edventure/settings.py
+++ edventure/settings.py
@@ -92,8 +92,13 @@ WSGI_APPLICATION = 'edventure.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'd6lrk1hu0nfbse',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'eqntqeozodgipz',
+        'PASSWORD': '4WAwN4LeS-QNX3xnNEsMP4wkQr',
+        'HOST': 'ec2-54-197-238-19.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 

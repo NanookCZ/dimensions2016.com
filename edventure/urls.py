430c6dd7f6f9a080e11d79d4a70a92b564860160
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-12 18:07:41 +0200

    urls

--- edventure/urls.py
+++ edventure/urls.py
@@ -1,21 +1,34 @@
-"""edventure URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+
+
+
+
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^accounts/', include ('allauth.urls')),
+    url(r'^$', 'videos.views.homepage', name='homepage'),
+    url(r'^s/$', 'videos.views.search', name='search'),
+    url(r'^content/', include('videos.urls')),
+    url(r'^test/(?P<slug>[\w-]+)/$', 'accounts.views.add_to_list', name='add_to_list'),
+    url(r'^favourite/$', 'accounts.views.user_content', name='favourite'),
+    url(r'^learned/$', 'accounts.views.user_content', name='learned'),
+    url(r'^later/$', 'accounts.views.user_content', name='later'),
+    url(r'^added-by-me/$', 'accounts.views.user_content', name='added-by-me'),
+    url(r'^user-detail/$', 'accounts.views.user_detail', name='user_detail'),
+    url(r'^(?P<id>\d+)/$', 'accounts.views.single_user', name='single-user'),
+    url(r'^events/$', 'events.views.event_view', name='events'),
+    url(r'^events/my$', 'events.views.user_event_view', name='my_events'),
+    url(r'^events/attending$', 'events.views.attending_view', name='attending'),
+    url(r'events/category/(?P<slug_cat>[\w-]+)/$', 'events.views.category_event', name='category-event'),
+    url(r'events/(?P<slug>[\w-]+)/$', 'events.views.event_detail', name='event-detail'),
+    url(r'^add-event/$', 'events.views.add_event', name='add_event'),
+    url(r'^edit-event/(?P<id>\d+)/$', 'events.views.edit_event', name='edit-event'),
+    url(r'^users/$', 'accounts.views.all_users', name='users'),
+    
+    
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

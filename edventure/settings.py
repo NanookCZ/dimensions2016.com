486ad6652bcc000360f709b017ca59e271127cd1
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-12 18:15:57 +0200

    a

--- edventure/settings.py
+++ edventure/settings.py
@@ -159,6 +159,7 @@ import os
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 

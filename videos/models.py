24df5b94b46ba6afd3f18e0b0367b650667e8e4e
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-14 14:50:00 +0200

    a

--- videos/models.py
+++ videos/models.py
@@ -1,6 +1,6 @@
 from __future__ import unicode_literals
 import urllib2
-import urllib.request
+import urllib
 from django.db import models
 from django.conf import settings
 from django.core.urlresolvers import reverse 
